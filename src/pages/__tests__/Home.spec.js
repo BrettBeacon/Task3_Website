afc1805bcabbb5c3cb51ea7b4d9293224f665e4c
BrettBeacon <brett.beacon@gmail.com>
2024-06-16 18:51:29 +1000

    removing testing files

--- src/pages/__tests__/Home.spec.js
+++
@@ -1,9 +0,0 @@
-const { mount } = require('@vue/test-utils');
-const Home = require('@/components/Home.vue');
-
-describe('Home', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Home);
-    expect(wrapper.vm).toBeTruthy();
-  });
-});
